143557e3bc303ebadfb9c4c4eda3353658360b65
Arip Hidayat <ariphidayat@hotmail.com>
2018-11-05 16:02:13 +0700

    for now, no need token enchancer

--- src/main/java/id/arip/config/AuthorizationServerConfig.java
+++ src/main/java/id/arip/config/AuthorizationServerConfig.java
@@ -7,11 +7,8 @@ import org.springframework.security.oauth2.config.annotation.configurers.ClientD
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
-import org.springframework.security.oauth2.provider.token.TokenEnhancerChain;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
-
-import java.util.Collections;
 
 /**
  * Created by Arip Hidayat on 11/5/2018.
@@ -31,13 +28,9 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 
     @Override
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
-        TokenEnhancerChain tokenEnhancer = new TokenEnhancerChain();
-        tokenEnhancer.setTokenEnhancers(Collections.singletonList(accessTokenConverter));
-
         endpoints
                 .tokenStore(tokenStore)
                 .accessTokenConverter(accessTokenConverter)
-                .tokenEnhancer(tokenEnhancer)
                 .authenticationManager(authenticationManager);
     }
 
